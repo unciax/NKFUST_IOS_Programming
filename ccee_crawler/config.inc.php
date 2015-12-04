717123ab31d12fc8cd73fd16a959e94e870acb16
Tzung Ru Wu <unciaq@gmail.com>
2015-12-05 00:08:17 +0800

    #add CCEE Crawler
    NKFUST CCEE Teacher Crawler

---
+++ ccee_crawler/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+	/*----------------------------------------------------
+		
+	NKFUST CCEE Teacher List v1.0
+
+	Programmer : UnciaX (u0251055@nkfust.edu.tw)
+	Last modified : 2015.12.02
+
+	----------------------------------------------------*/
+	
+	$ccee_domain = "http://www.ccee.nkfust.edu.tw";
+	$teacher_list = $ccee_domain . "/files/11-1021-2399.php";
+
+?>
